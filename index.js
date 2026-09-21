eac0ae92bae4e0e7bb428e6be23cbc868fc4530e
dan877141-hash <dan877141@gmail.com>
2026-09-21 18:28:17 -0500

    cmds

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const path = require('path');
 // DATA DIRECTORY
 // ======================================================
 
-const DATA_DIR = path.join(__dirname, 'data');
+const DATA_DIR = '/app/data';
 
 if (!fs.existsSync(DATA_DIR)) {
     fs.mkdirSync(DATA_DIR, {
